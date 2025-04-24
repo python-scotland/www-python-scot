b0337b8804ce571484cd50f0752f27ec3d8fdb0e
David <carmichaelits@gmail.com>
2025-04-24 18:19:45 +0100

    reorganise

--- app/__init__.py
+++ app/__init__.py
@@ -31,30 +31,30 @@ def create_app():
             title="Python Scotland"
         )
         g.head.set_favicon(
-            ico_icon_href="/favicon.ico",
-            png_icon_16_href="/favicon-16x16.png",
-            png_icon_32_href="/favicon-32x32.png",
-            png_icon_64_href="/favicon-64x64.png",
-            png_icon_96_href="/favicon-96x96.png",
-            png_icon_180_href="/favicon-180x180.png",
-            png_icon_196_href="/favicon-196x196.png",
-            png_apple_touch_icon_57_href="/apple-touch-icon-57x57.png",
-            png_apple_touch_icon_60_href="/apple-touch-icon-60x60.png",
-            png_apple_touch_icon_72_href="/apple-touch-icon-72x72.png",
-            png_apple_touch_icon_76_href="/apple-touch-icon-76x76.png",
-            png_apple_touch_icon_114_href="/apple-touch-icon-114x114.png",
-            png_apple_touch_icon_120_href="/apple-touch-icon-120x120.png",
-            png_apple_touch_icon_144_href="/apple-touch-icon-144x144.png",
-            png_apple_touch_icon_152_href="/apple-touch-icon-152x152.png",
-            png_apple_touch_icon_167_href="/apple-touch-icon-167x167.png",
-            png_apple_touch_icon_180_href="/apple-touch-icon-180x180.png",
-            png_mstile_70_href="/mstile-70x70.png",
-            png_mstile_270_href="/mstile-270x270.png",
-            png_mstile_310x150_href="/mstile-310x150.png",
-            png_mstile_310_href="/mstile-310x150.png",
+            ico_icon_href="/favicons/favicon.ico",
+            png_icon_16_href="/favicons/favicon-16x16.png",
+            png_icon_32_href="/favicons/favicon-32x32.png",
+            png_icon_64_href="/favicons/favicon-64x64.png",
+            png_icon_96_href="/favicons/favicon-96x96.png",
+            png_icon_180_href="/favicons/favicon-180x180.png",
+            png_icon_196_href="/favicons/favicon-196x196.png",
+            png_apple_touch_icon_57_href="/favicons/apple-touch-icon-57x57.png",
+            png_apple_touch_icon_60_href="/favicons/apple-touch-icon-60x60.png",
+            png_apple_touch_icon_72_href="/favicons/apple-touch-icon-72x72.png",
+            png_apple_touch_icon_76_href="/favicons/apple-touch-icon-76x76.png",
+            png_apple_touch_icon_114_href="/favicons/apple-touch-icon-114x114.png",
+            png_apple_touch_icon_120_href="/favicons/apple-touch-icon-120x120.png",
+            png_apple_touch_icon_144_href="/favicons/apple-touch-icon-144x144.png",
+            png_apple_touch_icon_152_href="/favicons/apple-touch-icon-152x152.png",
+            png_apple_touch_icon_167_href="/favicons/apple-touch-icon-167x167.png",
+            png_apple_touch_icon_180_href="/favicons/apple-touch-icon-180x180.png",
+            png_mstile_70_href="/favicons/mstile-70x70.png",
+            png_mstile_270_href="/favicons/mstile-270x270.png",
+            png_mstile_310x150_href="/favicons/mstile-310x150.png",
+            png_mstile_310_href="/favicons/mstile-310x150.png",
         )
         g.head.set_stylesheet(
-            href=url_for("static", filename="main.css")
+            href=url_for("static", filename="css/main.css")
         )
 
     @app.context_processor
